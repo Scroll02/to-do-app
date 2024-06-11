19eec2ddf45cb1e90e16fc8464035424ad28d0f7
Christian Joel Banquito <cjbanquito@gmail.com>
2024-06-11 09:58:08 +0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 const App = () => {
-  return <div>To-do-App</div>;
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+
+        <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+        />
+
+        <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
